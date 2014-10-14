7a60c6d42e4e3d9668b1325ff77eaeb0e3787d73
mirko <acme@gmx.com>
2014-10-14 01:15:10 -0400

    added RequireHttpAccess attribute as a global filters to enforce https acces for all the controllers

--- MvcOAuth2/Controllers/HomeController.cs
+++ MvcOAuth2/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace MvcOAuth2.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index()
---
+++ MvcOAuth2/Controllers/SampleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOAuth2.Controllers
+{
+    public class SampleController : Controller
+    {
+        //
+        // GET: /Sample/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
