e5de074008ed368d36cd875bf268c275e067e8ac
mirko <acme@gmx.com>
2014-10-31 22:31:07 -0400

    sample OAuth2 project

---
+++ MvcOAuth2/App_Start/Startup.Auth.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using MvcOAuth2.Models;
+using Owin.Security.Providers.LinkedIn;
+
+namespace MvcOAuth2
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Enables the application to validate the security stamp when the user logs in.
+                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: TimeSpan.FromMinutes(30),
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                }
+            });            
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Enables the application to remember the second login verification factor such as phone or email.
+            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            // This is similar to the RememberMe option when you log in.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "",
+            //   consumerSecret: "");
+
+            app.UseFacebookAuthentication(
+               appId: "664827780299018",
+               appSecret: "76f28d5e93769a1eec1a87ca175557ac");
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                //https://console.developers.google.com/project/nemamuka
+                ClientId = "429199303704-g8o9jl6qrp52b6obm1jkg8u4vb54pf0h.apps.googleusercontent.com",
+                ClientSecret = "9GeLa_jT-woSb2VafVneE9di"
+            });
+
+            app.UseLinkedInAuthentication(new LinkedInAuthenticationOptions() {
+                //https://www.linkedin.com/secure/developer?showinfo=&app_id=3545923&acc_id=1982853&compnay_name=mikNet&app_name=OAuth2TestM1k0
+                ClientId = "779y5gng16oygo",
+                ClientSecret = "Puq3y2vaMDDRpazn"
+            });
+            
+        }
+    }
+}
---
+++ MvcOAuth2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcOAuth2.MvcApplication" Language="C#" %>
